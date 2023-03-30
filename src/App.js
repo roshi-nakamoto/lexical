7f88b7149eaa84b8cdde7946be98fac48e4e7f37
roshi-nakamoto <98289392+roshi-nakamoto@users.noreply.github.com>
2023-03-30 18:04:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Editor from "./Editor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Rich Text Example</h1>
+      <p>Note: this is an experimental build of Lexical</p>
+      <Editor />
+    </div>
+  );
+}
